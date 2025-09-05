6cf635fb348e462790a96c3f1ac3f036074287e0
Alwson <leg.alien@yandex.ru>
2025-09-05 12:47:59 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -27,7 +27,6 @@ with sel2 as (
     left join products on sales.product_id = products.product_id
     group by employees.first_name, employees.last_name
 )
-
 select
     seller,
     FLOOR(average_income) as average_income
@@ -76,53 +75,53 @@ order by selling_month;
 --считает кол-во покупателей и выручку по месяцам
 
 
-WITH income AS (
-    SELECT
-        s.customer_id AS cust_id,
-        CONCAT(c.first_name, ' ', c.last_name) AS customer,
-        TO_CHAR(s.sale_date, 'YYYY-MM-DD') AS sale_date,
-        CONCAT(e.first_name, ' ', e.last_name) AS seller,
-        SUM(s.quantity * p.price) AS income
-    FROM sales s
-    LEFT JOIN customers c ON s.customer_id = c.customer_id
-    LEFT JOIN employees e ON s.sales_person_id = e.employee_id
-    LEFT JOIN products p ON s.product_id = p.product_id
-    GROUP BY c.first_name, c.last_name, s.sale_date, e.first_name, e.last_name, s.customer_id
+with income AS (
+    select
+        s.customer_id as cust_id,
+        CONCAT(c.first_name, ' ', c.last_name) as customer,
+        TO_CHAR(s.sale_date, 'YYYY-MM-DD') as sale_date,
+        CONCAT(e.first_name, ' ', e.last_name) as seller,
+        SUM(s.quantity * p.price) as income
+    from sales s
+    left join customers c on s.customer_id = c.customer_id
+    left join employees e on s.sales_person_id = e.employee_id
+    left join products p on s.product_id = p.product_id
+    group by c.first_name, c.last_name, s.sale_date, e.first_name, e.last_name, s.customer_id
 ),
-sn AS (
-    SELECT
+sn as (
+    select
         customer,
         sale_date,
         seller,
         income,
         cust_id,
         ROW_NUMBER() OVER (
-            PARTITION BY customer
-            ORDER BY sale_date
-        ) AS sale_number
-    FROM income
-    WHERE income = 0
+            PARTITION by customer
+            order by sale_date
+        ) as sale_number
+    from income
+    where income = 0
 )
-SELECT
+select
     customer,
     sale_date,
     seller
-FROM sn
-WHERE sale_number = 1
-ORDER BY cust_id;
+from sn
+where sale_number = 1
+order by cust_id;
 --показывает покупателей, первая покупка которых была в ходе проведения акций
-WITH income AS (
-    SELECT
-        s.customer_id AS cust_id,
-        CONCAT(c.first_name, ' ', c.last_name) AS customer,
-        TO_CHAR(s.sale_date, 'YYYY-MM-DD') AS sale_date,
-        CONCAT(e.first_name, ' ', e.last_name) AS seller,
-        SUM(s.quantity * p.price) AS income
-    FROM sales AS s
-    LEFT JOIN customers AS c ON s.customer_id = c.customer_id
-    LEFT JOIN employees AS e ON s.sales_person_id = e.employee_id
-    LEFT JOIN products AS p ON s.product_id = p.product_id
-    GROUP BY
+with income as (
+    select
+        s.customer_id as cust_id,
+        CONCAT(c.first_name, ' ', c.last_name) as customer,
+        TO_CHAR(s.sale_date, 'YYYY-MM-DD') as sale_date,
+        CONCAT(e.first_name, ' ', e.last_name) as seller,
+        SUM(s.quantity * p.price) as income
+    from sales as s
+    left join customers as c on s.customer_id = c.customer_id
+    left join employees as e on s.sales_person_id = e.employee_id
+    left join products as p on s.product_id = p.product_id
+    group by
         c.first_name,
         c.last_name,
         s.sale_date,
@@ -131,26 +130,27 @@ WITH income AS (
         s.customer_id
 ),
 
-sn AS (
-    SELECT
+sn as (
+    select
         customer,
         sale_date,
         seller,
         income,
         cust_id,
         ROW_NUMBER() OVER (
-            PARTITION BY customer
-            ORDER BY sale_date
-        ) AS sale_number
-    FROM income
-    WHERE income = 0
+            PARTITION by customer
+            order by sale_date
+        ) as sale_number
+    from income
+    where income = 0
 )
 
-SELECT
+select
     customer,
     sale_date,
     seller
-FROM sn
-WHERE sale_number = 1
-ORDER BY cust_id;
+from sn
+where sale_number = 1
+order by cust_id;
 --показывает покупателей, первая покупка которых была в ходе проведения акций
+
